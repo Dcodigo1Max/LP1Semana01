95455dc9d3afb33887af7f961c421f4f4aa3f3c6
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-02-21 16:33:54 +0000

    Inteiros

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+            int i = 20;
+            int j = 10;
+            int p = 2;
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+            Console.WriteLine(p);
+
+        }
+    }
+}
