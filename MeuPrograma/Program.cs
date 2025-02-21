062cc7aa0fed8b5e54f333e515c50fb31e6b7b69
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-02-21 10:39:38 +0000

    Preencher Tags

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -4,6 +4,10 @@ namespace MeuPrograma
 {
     public class Program
     {
+        /// <summary>
+        /// Changes to file for Exer2
+        /// </summary>
+        /// <param name="args"> This is for exercice 2</param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
