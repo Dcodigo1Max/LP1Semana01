0f2e4dad33ff0994461825f15a3015090e0a85cb
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-02-21 18:03:23 +0000

    Entrega

---
+++ Smiley/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Smiley
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding=Encoding.UTF8;
+            Console.WriteLine("Hello LP1!");
+
+            //Variaveis
+            string strNumero1;
+
+            //Input
+            Console.WriteLine("Insere codigo: ");
+            strNumero1 = Console.ReadLine();
+
+            //Converter 
+            Convert.ToInt32(strNumero1);
+            Console.Write(strNumero1);
+            
+        
+        }   
+    }
+}
